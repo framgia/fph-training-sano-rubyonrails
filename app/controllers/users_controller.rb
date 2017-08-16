d95e3bb850cd47306ce52f620ba0bcb2af648fa9
sanoyo <sa00380304yo@gmail.com>
2017-08-16 17:47:37 +0800

    Create Login Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :logged_in_user, only: [:new]
+
     def index
     end
 
@@ -13,6 +15,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            log_in @user
             flash[:success] = "Create Infomation Successfully"
             redirect_to root_url
         else
@@ -33,4 +36,9 @@ class UsersController < ApplicationController
         def user_params
             params.require(:user).permit(:name, :email, :password, :password_confirmation, :avator)
         end
+
+        def logged_in_user
+            redirect_to(root_url) if session[:user_id]
+        end
 end
+
